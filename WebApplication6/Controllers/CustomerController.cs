581b3bcc680c4bce64bd81af6382454b6f4ae980
admin <admin@CHETUIWK1240>
2022-03-04 16:27:40 +0530

    Add project files.

---
+++ WebApplication6/Controllers/CustomerController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    public class CustomerController : Controller
+
+    {
+        private ApplicationDbContext dbContext;
+
+        public CustomerController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Location>
+                Locations = dbContext.Locations.ToList();
+            return View(Locations);
+        }
+        public IActionResult CustomerList (int id)
+        {
+            List<Customer>
+                Customers
+                = dbContext.Customers.Where
+                (e => e.Location.Id == id).ToList();
+            return View(Customers);
+        }
+        public IActionResult ShowAllCust()
+        {
+            var customers = dbContext.Customers.ToList();
+            return View(customers);
+        }
+        public IActionResult Create()
+        {
+      
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            dbContext.Customers.Add(customer);
+            dbContext.SaveChanges();
+
+            return RedirectToAction("ShowAllCust");
+        }
+        public IActionResult Delete(int?id)
+        {
+            var data = dbContext.Customers.FirstOrDefault(x => x.Id == id);
+            return View(data);
+        }
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var data = dbContext.Customers.FirstOrDefault(x =>x.Id == id);
+            dbContext.Customers.Remove(data);
+            dbContext.SaveChanges();
+            return RedirectToAction("ShowAllCust");
+        }
+        public IActionResult Edit(int id)
+        {
+
+         var emp=   dbContext.Customers.FirstOrDefault(x => x.Id == id);
+
+            return View(emp);
+        }
+        [HttpPost]
+        public IActionResult Edit(Customer customer)
+        {
+
+            dbContext.Customers.Update(customer);
+            dbContext.SaveChanges();
+            return RedirectToAction("ShowAllCust");
+        }
+
+    }
+}
---
+++ mvc.sql
@@ -0,0 +1,17 @@
+﻿
+insert into Locations values
+('Noida', ' this is a best place'),
+('Delhi', ' this is a good place'),
+('Lucknow', ' this is a best place'),
+('Bijnor', ' this is a best place'),
+('meerut', ' this is a best place');
+select*from Locations;
+insert into  Customers values
+('Anchal singh','anchal@gmail.com','125749',1),
+('arif','arif@gmail.com','125749',2),
+('Ankit singh','ankit@gmail.com','125749',1),
+('Shiksha singh','shiksha@gmail.com','125749',3),
+('azhar','azhar@gmail.com','125749',1),
+('vishal','vishal@gmail.com','125749',3);
+
+
